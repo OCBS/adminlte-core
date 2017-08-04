7925bd81fe0609e3d85120a4f4dff6ae52cf0da3
Stanislav Fesenko <iamdeuterium@gmail.com>
2017-08-04 08:07:58 +0300

    Update

--- app/controllers/concerns/adminlte/base_controller.rb
+++ app/controllers/concerns/adminlte/base_controller.rb
@@ -5,5 +5,11 @@ module Adminlte
     included do
       layout 'adminlte/layouts/application'
     end
+
+    module ClassMethods
+      def local_prefixes
+        ['adminlte/application', controller_path]
+      end
+    end
   end
 end
